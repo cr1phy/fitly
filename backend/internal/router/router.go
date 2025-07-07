b2a992e4adf383fc8764fcaf5a3fb013d83afee8
Sergey Krifov <cr1phy@mail.ru>
2025-07-07 11:46:44 +0400

    some changes in backend

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -40,6 +40,34 @@ func addProduct(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Successfully created!"})
 }
 
+func getDishes(c *gin.Context) {
+	filter := c.Query("filter")
+
+	result := []models.Dish{}
+	models.DB.Model(&models.Dish{}).Where("name LIKE ?", filter+"%").Group("name").Find(&result)
+
+	if len(result) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Not found"})
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func addDish(c *gin.Context) {
+	var d models.Dish
+	if err := c.ShouldBindBodyWithJSON(&d); err != nil {
+		log.Panicln("something went wrong with body:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Something went wrong"})
+		return
+	}
+
+	if err := models.DB.Create(&d); err != nil {
+		log.Panicln("something went wrong with creating product:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Something went wrong"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully created!"})
+}
+
 func InitRouter() *gin.Engine {
 	r := gin.Default()
 
@@ -49,6 +77,8 @@ func InitRouter() *gin.Engine {
 	r.GET("/", status)
 	r.GET("/products", getProduct)
 	r.POST("/products", addProduct)
+	r.GET("/dishes", getDishes)
+	r.POST("/dishes", addDish)
 
 	return r
 }
