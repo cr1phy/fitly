c1a8ddcd3edd246b174c8a3ff5745faa5cb8a083
Sergey Krifov <cr1phy@mail.ru>
2025-07-08 16:26:13 +0400

    some changes in backend

--- backend/cmd/fitly/main.go
+++ backend/cmd/fitly/main.go
@@ -8,38 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func main() {
-// 	ctx := context.Background()
-
-// 	server := &http.Server{Addr: ":8080", Handler: router.InitHandler(db)}
-// 	serverCtx, serverCancel := context.WithCancel(ctx)
-
-// 	sig := make(chan os.Signal, 1)
-// 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
-// 	go func() {
-// 		<-sig
-// 		shutdownCtx, shutdownCancel := context.WithTimeout(serverCtx, 30*time.Second)
-// 		defer shutdownCancel()
-
-// 		go func() {
-// 			<-shutdownCtx.Done()
-// 			if shutdownCtx.Err() == context.DeadlineExceeded {
-// 				log.Fatalln("graceful shutdown timed out.")
-// 			}
-// 		}()
-
-// 		err := server.Shutdown(serverCtx)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		serverCancel()
-// 	}()
-
-// 	log.Println("server started")
-// 	log.Fatalln(server.ListenAndServe())
-// }
-
 func main() {
 	gin.SetMode("release")
 
